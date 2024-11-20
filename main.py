8571c570609bf546a2b1d89c0ae7f839b4891396
Kinsburskiy <9104634556ak@gmail.com>
2024-11-20 11:22:14 +0300

    Add file

---
+++ main.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    print(9 ** 0.5 * 5) # 1st program
+    print(9.99 > 9.98 and 9.98 != 1000.1) # 2st program
+    print((2 * 2 + 2) == ((2 + 2) * 2)) # 3st program
+    print(int((float('123.456')) * 10) % 10) # 4st program
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
