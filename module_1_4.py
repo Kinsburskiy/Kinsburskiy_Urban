08e6f9f837e3cc11d5704c2d9cc4d9452db39f4c
Kinsburskiy <9104634556ak@gmail.com>
2024-11-22 00:21:45 +0300

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -1,5 +1,7 @@
 my_string = input('В каком учебном заведении Вы учитесь программированию на Python? ')
 print(my_string) # строка введённого текстового ответа
+Number_of_characters = len(my_string) # количество символов введённого текста
+print('Количество символов в ответе:', Number_of_characters)
 print(my_string.upper()) # строка в верхнем регистре (прописные буквы)
 print(my_string.lower()) # строка в нижнем регистре (строчные буквы)
 print(my_string.replace(' ', '')) # строка без пробелов
